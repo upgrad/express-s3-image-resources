0c7b6191bc14231153e1625902430b1c2696c7e1
Mohit Karekar <karekar.mohit@gmail.com>
2020-06-17 16:14:33 +0530

    Updated S3 config

--- dist/es5/lib/config/s3.js
+++ dist/es5/lib/config/s3.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
@@ -6,7 +6,7 @@ var AWS = require('aws-sdk');
 var awsS3Client = void 0;
 
 var isConfig = function isConfig(configOrInstance) {
-	if ((typeof configOrInstance === 'undefined' ? 'undefined' : _typeof(configOrInstance)) == "object" && configOrInstance.accessKeyId && configOrInstance.secretAccessKey && configOrInstance.region) {
+	if ((typeof configOrInstance === "undefined" ? "undefined" : _typeof(configOrInstance)) == "object" && configOrInstance.accessKeyId && configOrInstance.secretAccessKey && configOrInstance.region) {
 		return true;
 	}
 	return;
@@ -16,7 +16,7 @@ module.exports = function (configOrInstance) {
 	if (!configOrInstance) return awsS3Client;
 	if (isConfig(configOrInstance)) {
 		AWS.config.update(configOrInstance);
-		awsS3Client = new AWS.S3({ apiVersion: '2006-03-01' });
+		awsS3Client = new AWS.S3();
 	} else {
 		awsS3Client = configOrInstance;
 	}
